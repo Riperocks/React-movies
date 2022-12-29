837aeb6688dd1087917feac96f8391f5417a3a0c
Kirill Komarov <riperocks98@gmail.com>
2022-12-29 18:03:17 +0300

    upload header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,20 @@
-function Header () {
-    return <nav className="cyan darken-3">
-        <div className="nav-wrapper">
-        <a href="!#" className="brand-logo">React Movies</a>
+function Header() {
+  return (
+    <nav className="cyan darken-3">
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">
+          React Movies
+        </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="https://github.com/Riperocks/React-movies/tree/gh-pages">Repo</a></li>
+          <li>
+            <a href="https://github.com/Riperocks/React-movies/tree/gh-pages">
+              Repo
+            </a>
+          </li>
         </ul>
-        </div>
+      </div>
     </nav>
+  );
 }
 
-export {Header}
+export { Header };
